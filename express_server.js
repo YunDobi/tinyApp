b1970e4e1bc45350c383d471433ec27ed2e75bbd
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-12-06 18:21:13 -0800

    short and long URL

--- express_server.js
+++ express_server.js
@@ -19,7 +19,7 @@ app.get("/urls.json", (req, res) => {
 });//Json file in object
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});//html file with bold
+});//html file with boldlongURL
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -27,7 +27,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
